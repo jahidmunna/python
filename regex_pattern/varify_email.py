37b1bf4ad956e6d7065c103bb7dbef9df65f9077
Munna <jahidulislam.munna@rakuten.com>
2022-01-15 14:58:13 +0900

    added email varification using regex

---
+++ regex_pattern/varify_email.py
@@ -0,0 +1,9 @@
+import re
+
+EMAIL_PATTERN = "[a-zA-Z]+[0-9]*@[a-zA-Z]+\.(com|edu|net)"
+
+while True:
+    user_input =str(input("Enter E-Mail: "))
+    is_Valid = "Valid" if re.search(EMAIL_PATTERN, user_input) else "Not Valid"
+
+    print("[", user_input, "]\t", is_Valid)
